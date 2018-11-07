52b19a38ce50c12321d194b13660ed4bcb4447d2
BolajiOlajide <bolaji.olajide@andela.com>
2018-11-07 07:08:59 +0100

    upload package to PyPi

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-from distutils.core import setup
+from setuptools import setup
 
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -20,16 +19,21 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='ng_banks',
     packages=['ng_banks'],
-    version='1.0',  # Ideally should be same as your GitHub release tag varsion
+    version='1.0.0',
     long_description=long_description,
 
     description='Python Implementation for Lightweight Zero dependency npm \
     package to get list of banks in Nigeria (Recognized by CBN)',
 
     author='Bolaji Olajide',
-    author_email='',
+    author_email='cooproton@gmail.com',
     url='https://github.com/BolajiOlajide/ng_banks',
     download_url='',
+    long_description_content_type="text/markdown",
     keywords=['banks', 'nigeria', 'nigerian', 'cbn'],
-    classifiers=[]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
